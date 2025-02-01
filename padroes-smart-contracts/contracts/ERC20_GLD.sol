d2794e31aad98f59e02a542b31d5c420116f4d0f
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-02 00:11:00 +0100

    creating ERC token to make payments

--- padroes-smart-contracts/contracts/ERC20_GLD.sol
+++ padroes-smart-contracts/contracts/ERC20_GLD.sol
@@ -3,8 +3,26 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+
 contract WrapedGold_ERC20 is ERC20 {
-   constructor(uint256 initialSupply) ERC20("WrapedGold", "WGD") {
+
+   address despachanteAddress;
+
+   constructor(uint256 initialSupply, address _despachanteAddress) ERC20("WrapedGold", "WGD") {
       _mint(msg.sender, initialSupply);
+
+      despachanteAddress = _despachanteAddress;
    }
+
+   function despachanteTransfer(address from, address to, uint256 amount) public onlyDespachant returns (bool success) {
+      _transfer(from, to, amount);
+   }
+
+   modifier onlyDespachant() { // modifier to control access
+      require(msg.sender == despachanteAddress,"Only despachante can call this function");
+      _; // means continue
+   }
+
 }
+
+
